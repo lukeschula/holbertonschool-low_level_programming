3b26e8052d90a4a5cce4efda70dd5f3e3c30ab32
lukeschula <schulaluke@gmail.com>
2023-05-30 17:36:48 -0700

    commit
    bgycfg

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (1 <= 0)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}	
+	_putchar('\n');
+	i++;
+	}
+}
