2d2cce2226e799d26827f8e2dc56e84b0f013bbb
lukeschula <schulaluke@gmail.com>
2023-05-30 17:42:50 -0700

    dchhc

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print the alphabet 10 times
- * Return: void
+ * print_alphabet_x10 - print the alphabet 10 time
+ * Descreption: using _putchar
+ * Return: Always 0.
  */
-
 void print_alphabet_x10(void)
 {
 	char c;
-	int i = 0;
+	int i;
 
-	while (1 <= 0)
+	for (i = 1; i <= 10; i++)
 	{
-	for (c = 'a'; c <= 'z'; c++)
-	{
-	_putchar(c);
-	}	
-	_putchar('\n');
-	i++;
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
 }
