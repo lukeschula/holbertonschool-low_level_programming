afb14b07ac2809c9c7e9f61a57a96d988014fec9
lukeschula <schulaluke@gmail.com>
2023-06-02 05:48:40 -0700

    fbhhfbbh

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,17 @@
 #include "main.h"
-
 /**
- * print_numbers - display all number from 0 to 9 followed by new line.
+ * print_most_numbers - print all number from 0 to 9 expext 2 and 4.
  *
- *Return: void
+ * Return:void
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
+int i;
 
-	int x;
-
-	for (x = 0; x <= 9; x++)
+	for (i = 48; i <= 57; i++)
 	{
-	_putchar(x + '0');
+		if ((i != 50) && (i != 52))
+			_putchar(i);
 	}
-	_putchar('\n');
+_putchar('\n');
 }
