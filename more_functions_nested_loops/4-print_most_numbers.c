6521a184fcdcc2aa3410536a5aa0e299040df2ce
lukeschula <schulaluke@gmail.com>
2023-06-02 05:41:13 -0700

    dswqcc

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.hi"
+/**
+ * print_most_numbers - print all number from 0 to 9 expext 2 and 4.
+ *
+ * Return:void
+ */
+void print_most_numbers(void)
+	
+{
+	int x = 0;
+
+	for (; <= 9; x++)
+	{
+	if (x == 2 || x== 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(x + '0');
+	}
+	}
+
+	_putchar('\n');
+}
