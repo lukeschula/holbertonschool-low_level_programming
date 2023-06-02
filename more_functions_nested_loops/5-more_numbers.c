8fa9e5ac851bc4b9d12c0c14df4ceba93224685a
lukeschula <schulaluke@gmail.com>
2023-06-02 09:27:03 -0700

    gDFF

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_more_numbers - print all number from 0 to 9 expext 2 and 4.
+ * more_numbers - print 10 times from 0 to 14
  *
  * Return:void
  */
@@ -8,14 +8,16 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i<= 10; i++)
+	for (i = 1; i < 10; i++)
 	{
-		for (j = 0; j<= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{	
 			if (j >= 10)
-				_putchar('1');	
+			{
+				_putchar('1');
+			}
 			_putchar (j % 10 + '0');
-		}	
+		}		
 		_putchar('\n');
 	}	
-}
+}	
