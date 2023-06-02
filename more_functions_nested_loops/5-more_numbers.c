0fab701632f91721ce22a4193503828d0fed0128
lukeschula <schulaluke@gmail.com>
2023-06-02 09:15:28 -0700

    ghjgehjfhj

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * print_most_numbers - print all number from 0 to 9 expext 2 and 4.
+ * print_more_numbers - print all number from 0 to 9 expext 2 and 4.
  *
  * Return:void
  */
 void more_numbers(void)
 {
 	int i, j;
+
 	for (i = 1; i<= 10; i++)
 	{
-	for (j = 0; j<= 14; j++)
-	{
-	if (j >= 10)
-	_putchar('1');	
-	_putchar (j % 10 + '0');
-	}
-	_putchar('\n');
+		for (j = 0; j<= 14; j++)
+		{	
+			if (j >= 10)
+				_putchar('1');	
+			_putchar (j % 10 + '0');
+		}	
+		_putchar('\n');
 	}	
 }
