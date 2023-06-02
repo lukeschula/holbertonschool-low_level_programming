c2eca44c06c0d28c7d131c905aa3ce1cc5146ea2
lukeschula <schulaluke@gmail.com>
2023-06-02 09:36:55 -0700

    gdeeewv

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,26 @@
 #include "main.h"
+
 /**
- * more_numbers - print 10 times from 0 to 14
+ * more_numbers - print 10 time from 0 to 14
  *
- * Return:void
+ *Return:void
  */
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
-{	
+		{
 			if (j >= 10)
 			{
 			_putchar('0' + j / 10);
 			}
-			_putchar ('0' + j % 10);
-}		
-		_putchar('\n');
-}	
+			_putchar('0' +  j % 10);
+
+
+		}
+	_putchar('\n');
+	}
 }
