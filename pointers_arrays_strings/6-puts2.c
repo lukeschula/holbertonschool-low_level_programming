6904e0c264b757b35c5fcda42c6535e7b8fc47ae
lukeschula <schulaluke@gmail.com>
2023-06-05 08:56:51 -0700

    c ffh

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,31 @@
- #include "main.h"
-
+#include "holberton.h"
+#include <stdio.h>
 /**
- * puts2 - Write a function that prints every other character of a string, 
- *         starting with the first character, followed by a new line.
+ * rev_string - This fuction print a string reverser
  *
- * @s: This pointer point to a char or string
- *
- *
+ *@s: this is the pointer that point to a string
  */
 
-void puts2(char *s)
+void rev_string(char *s)
 {
-	int a;
+	int a, len;
 
-	for (a = 0; s[a] != '\0'; a++)
+	char *begin, *end = s;
+
+	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
 	{
-		if (a % 2 == 0)
-		{
-			_putchar(s[a]);
-		}
+		end++;
 	}
-	_putchar('\n');
+	len = a + 1;
+	begin = s;
+	for (a = 0; a < len / 2; a++)
+	{
+		char x;
+		x = *end;
+		*end = *begin;
+		*begin = x;
+		begin++;
+		end--;
+	}
+	end[len + 1] = '\0';
 }
