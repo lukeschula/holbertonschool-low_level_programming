725e5ae8231c4575b0f7033c4e179111e63eba03
lukeschula <schulaluke@gmail.com>
2023-07-17 18:40:16 -0700

    ff

--- main.h
+++ main.h
@@ -1,12 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *create_array(unsigned int size, char c);
-int _putchar(char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
