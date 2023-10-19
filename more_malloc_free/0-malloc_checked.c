d1c86487c493bec4618518bf0f9d9d26930861e8
root <schulaluke@gmail.com>
2023-10-19 08:48:46 -0700

    added describtion and prototype to file 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/* 
+ * malloc_checked: allocates memory 
+ *
+ * b: unassigned integer
+ *
+ * Return: pointer to alloacted memory 
+ */
+void *malloc_checked(unsigned int b)
+{	
+	
