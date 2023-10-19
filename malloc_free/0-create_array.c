f097392f0ba0126b8586af523fb6d90ac95f13d4
root <schulaluke@gmail.com>
2023-10-19 08:04:13 -0700

    added describtion and prototype to file 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * create an array
+ * size: size input 
+ * c: char 
+ * Return: 0
+ */
+
+char *create_array(unsigned int size, char c)
+{	
+	
