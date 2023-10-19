99863fd07a99540eead62c9e677d873d02114a03
root <schulaluke@gmail.com>
2023-10-19 08:12:43 -0700

    added a description and prototype to file 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
 #include <stdlib.h> 
-
 #include "main.h"
+/**
+ * _strdup: duplicates a string into newly allocated space in memory
+ * 
+ * str: string to duplicate
+ *
+ * Return: pointer to a new string
+ */
+char *_strdup(char *str)
+{	
+	
