7b21bda6fd3b87c8d980935a36fe2ab991e51d20
root <schulaluke@gmail.com>
2023-10-19 08:25:46 -0700

    added desciption and prototype to file 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,3 +1,14 @@
 #include "main.h"
-
 #include <stdlib.h>
+#include <stdlib.h>
+/**
+ * str_concat: concatenates two strings
+ *
+ * s1: first string
+ * s2: string to add at the end of first string
+ *
+ * Return: pointer to newly allocated string concatenation 
+ */
+char *str_concat(char *s1, char *s2)
+{	
+
