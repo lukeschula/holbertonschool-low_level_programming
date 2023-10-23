3e04db535faca32959412b3eb787d197d225f1bc
root <schulaluke@gmail.com>
2023-10-23 14:30:03 -0700

    1st edit to code

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
  * create an array
  * size: size input 
@@ -10,4 +9,18 @@
 
 char *create_array(unsigned int size, char c)
 {	
-	
+	char *array;
+	unsigned int i;
+
+	array = malloc(sizeof(char) *size);
+
+	if (array == NULL)
+	{
+		return(NULL);
+	}
+	for (i = 0; i < size; i++)
+	{	
+		array[i] = c;
+
+	} return (array);
+}
