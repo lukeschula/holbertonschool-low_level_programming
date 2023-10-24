1461df8a2a554dc4674a7912021b742069c79448
root <schulaluke@gmail.com>
2023-10-23 19:40:19 -0700

    betty style fix

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,18 +11,18 @@
  */
 
 char *create_array(unsigned int size, char c)
-{	
+{
 	char *array;
 	unsigned int i;
 
-	array = malloc(sizeof(char) *size);
+	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
-	{	
+	{
 		array[i] = c;
 
 	} return (array);
