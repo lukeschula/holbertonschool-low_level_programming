0bbe04be078590493b3732e60084926a5d3143f3
root <schulaluke@gmail.com>
2023-10-23 19:30:40 -0700

    betty style fix

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,10 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create an array
- * size: size input 
- * c: char 
- * Return: 0
+ * *create_array - program startup
+ * @size: int being evaluated
+ * @c: char being evaluated
+(*
+ * Description: creates an array of chars
+ * and initializes it with a specific char)?
+ * Return: return (0) is the required function signature
  */
 
 char *create_array(unsigned int size, char c)
