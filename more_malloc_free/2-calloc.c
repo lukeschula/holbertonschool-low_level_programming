2bcc3c86809eab177fcdf9191b9a19e67e0f6914
root <schulaluke@gmail.com>
2023-10-23 18:32:45 -0700

    1st edit to file 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,4 +1,35 @@
 #include "main.h"
-
 #include <stdlib.h>
+/**
+ * *_calloc - program startup
+ * @nmemb: first int being evaluated
+ * @size: second int being evaluated
+ * 
+ * Description: allocates memory for an array using malloc)?
+ * Return: return (0) is the required function signature
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *yoyo;
+	char *coco;
+	unsigned int rep;
 
+	if (nmemb == 0 || size == 0)
+	{
+		return(NULL);
+	}
+
+	yoyo = malloc(size * nmemb);
+
+	if (yoyo == NULL)
+	{
+		return(NULL);
+	}
+	coco = yoyo;
+
+	for (rep = 0; 0 < (size * nmemb); rep++)
+	{
+		coco[rep] = '\0';
+	}
+	return (yoyo);
+}
