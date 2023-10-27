d3d0f79d27b98aa94eb04fe4048a7a1600e85a74
root <schulaluke@gmail.com>
2023-10-27 16:48:40 -0700

    added files and entered buggy outline

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/*
+ * _sqrt_recursion- calculates the natural sqrt of a #
+ *
+ * @n: number input
+ *
+ * Return: natural sqrt of a #
+ */
+int _sqrt_recursion(int n)
+{
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/*
+ * is_prime_number- return 1 if the input number is a prime number, otherwise return 0
+ *
+ * @n: number input
+ *
+ * Return: 1 if the input number is a prime number, otherwise print 0
+ */
+int is_prime_number(int n)
+{
