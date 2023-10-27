92afecb9e0bd8d4a23cfc3662d274cc866017cba
root <schulaluke@gmail.com>
2023-10-27 10:59:58 -0700

    fixing betty code style

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * _strlen_recursion - Returns the length of a string
+ * @s: pointer to the string to be measured
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s);
+{
+	if(*s);
+
+		return (_strlen_recursion(s + 1) + 1);
+
+	return (0);
+}
