b6914abaa4a748786d8bd74ea16fc7af9db57af6
root <schulaluke@gmail.com>
2023-10-27 10:14:43 -0700

    added file 1, fixed bugs as well

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * _print_rev_recursion - program start
+ * @s: pointer 
+ *
+ * Print a string in reverse
+ * Return 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
