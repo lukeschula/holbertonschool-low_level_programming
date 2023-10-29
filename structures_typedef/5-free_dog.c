112f7ed69aeaa3ac016dd9f1280c1995b936a8a7
root <schulaluke@gmail.com>
2023-10-29 15:34:39 -0700

    second part of code debugg

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,34 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function to free dog
+ * @d: pointer to structure dog_t
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL);
+	
+		return;
+	
+	else if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+	}
+	else if (d->name == NULL)
+	{
+		free(d->owner);
+		free(d);
+	}
+	else
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+
+
+}
+
+	
