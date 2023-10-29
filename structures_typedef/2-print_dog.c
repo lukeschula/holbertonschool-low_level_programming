85c1357e27522795c55c730a9f8c26c4a0214d36
root <schulaluke@gmail.com>
2023-10-29 11:34:59 -0700

    simplified and debugged code in file 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints struct dog
+ *
+ * @d: pointer to struct dog
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name == NULL ? "(nil)" : d->name));
+
+		printf("Age: %f\n" , d->age);
+
+		printf("Owner: %s\n" , ( d->owner == NULL ? "(nil)" : d->owner));
+	}
+}
