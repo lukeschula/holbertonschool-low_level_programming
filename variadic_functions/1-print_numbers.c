389f20d3ad8fd4a55a353d02dac452b258ba5017
root <schulaluke@gmail.com>
2023-10-30 08:29:06 -0700

    2nd attempt to fix buggy code

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - function that prints numbers, followed by a new line
+ *
+ * @separator: pointer to the string to be printed
+ * @n: number of strings passed through the argument
+ * 
+ * Return: printed numbers, followed by a new line
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list man;
+	char *ci;
+	unsigned int in;
+
+	for (in = 0; in < n; in++;)
+	{
+		ci = va_arg(man, char *);
+
+		if (ci == NULL)
+			printf("(nil)")
+
+		else 
+			printf("%s", ci);
+
+		if (in != (n - 1) && seperator != NULL)
+			prinf("%s", seperator);
+	}
+
+	printf("\n");
+}
