654c389a0336ebef6e169d1c19b577b2cea8f445
root <schulaluke@gmail.com>
2023-10-30 19:15:54 -0700

    betty fix

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,13 +1,17 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 /**
- * Description: array_iterator function given a parameter
+ * array_iterator - executes a function given as a parameter
  *
  * @array: pointer to the array about to be executed
  * @size: size of the array
  * @action: a pointer to the function you need to use
  *
  * Return: void
+ *
+ * Description: function array_iterator is a function
+ * that executes another function
+ *
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
