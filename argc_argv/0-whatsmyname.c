40433b921ed277412fc464f84c7964033235b20f
root <schulaluke@gmail.com>
2023-10-31 18:15:19 -0700

    betty fix

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 /**
  * main - hat prints its name, followed by a new line.
- * @argc: argument count 
+ * @argc: argument count
  * @argv: argument vector
+ *
+ * Return: 0
+ *
+ * Desrciption: return value passed through
+ * the main arg should be 0
  */
 int main(int argc, char *argv[])
 {
