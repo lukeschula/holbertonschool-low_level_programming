57d8b33d9011f91f5b10be9edad41c02595f665e
root <schulaluke@gmail.com>
2023-10-31 17:11:48 -0700

    corrected answer to file 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - hat prints its name, followed by a new line.
+ * @argc: argument count 
+ * @argv: argument vector
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	{
+		return (0);
+	}
+}
