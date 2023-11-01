6077bd705d6e1004acef8a7420bbe5da425e566d
root <schulaluke@gmail.com>
2023-10-31 21:30:31 -0700

    finally solved file 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int prime_checker(int n, int o);
 /*
  * check_num- function to help check if the integer input is a prime number
  *
@@ -7,38 +8,32 @@
  *
  * Return: 1 if the input is a prime number, otherwise 0 if not prime 
  */
-int check_num(int o, int p)
-{
-	int x, y;
-
-	x = o / p;
-	y = o % p;
-
-	--p;
-
-	if (y == 0 && p < o && x != o)
-	{
-		return (0);
-	}
-	else if (y == 0 && p == 1 && x == o)
-	{
-		return (1);
-	}
-	return (check_num(o, p));
-}
-
-/*
- * is_prime_number- return 1 if the input number is a prime number, otherwise return 0
- *
- * @n: number input
- *
- * Return: 1 if the input number is a prime number, otherwise print 0
- */
 int is_prime_number(int n)
 {
 	if ( n <= 1)
 	{
 		return (0);
 	}
-	return (check_num(n, n));
+	else if (prime_checker(n, n / 2) > 0 )
+	{
+		return (1);
+	}
+	return (0);
 }
+
+int prime_checker(int n, int o)
+{
+	if (o == 1)
+	{
+		return (1);
+	}
+	if (n % o == 0)
+	{
+		return(0);
+	}
+	else
+	{
+		return (prime_checker(n, o - 1 ));
+	}
+}
+
