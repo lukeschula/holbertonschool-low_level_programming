3eaf82ca0be7d35461c917ea5b7f44d9347259ac
root <schulaluke@gmail.com>
2023-10-31 21:01:06 -0700

    betty fix

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-/** 
+/**
  * free grid: function from previous problem
  *
- * grid: 2 dimensional array to free 
- * height: input height 
+ * grid: 2 dimensional array to free
+ * height: input height
  *
  * Return: grid that has been freed
  */
 void free_grid(int **grid, int height)
-{	
+{
 	int q;
-	
+
 	for (q = 0; q < height; q++)
 	{
 		free(grid[q]);
 	}
 	free(grid);
-}	
+}
