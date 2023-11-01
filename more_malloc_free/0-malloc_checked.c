da2881df499430f258173524edd8e6a224cdc857
root <schulaluke@gmail.com>
2023-10-31 18:33:00 -0700

    betty fix

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,22 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/* 
- * malloc_checked: allocates memory 
+/*
+ * malloc_checked - returns a pointer to the  allocates memory
  *
  * b: unassigned integer
  *
- * Return: pointer to alloacted memory 
+ * Return: pointer to alloacted memory
+ *
+ * Description: malloc_checked function should cause normal process termination
+ * with a status value of 98. return pointer
+ *
  */
 void *malloc_checked(unsigned int b)
-{	
+{
 	void *t;
 
 	t = malloc(b);
 
 	if (t == NULL)
-	{	
+	{
 		exit(98);
-	}	
+	}
 	return (t);
-}	
+}
