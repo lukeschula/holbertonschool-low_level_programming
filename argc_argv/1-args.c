c32f767820bcf9cab09df9eb928073782344574f
root <schulaluke@gmail.com>
2023-10-31 18:17:37 -0700

    betty fix

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - program that prints the number of arguments passed into it.
- * 
- * @argc: argument count 
+ *
+ * @argc: argument count
  * @argv: argument vector
  *
  * Return: 0
@@ -13,9 +13,9 @@ int main(int argc, char **argv)
 	int y = 0;
 
 	for (x = argc - 1; *(argv + x) != *argv; x--)
-		
+
 		++y;
 	printf("%d\n", y);
 
-	return(0);
+	return (0);
 }
