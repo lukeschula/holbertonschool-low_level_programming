2b9774a62d67d88d22d0ff0f4636dd6b24592278
root <schulaluke@gmail.com>
2023-10-31 21:35:18 -0700

    betty fix

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,17 +4,17 @@ int prime_checker(int n, int o);
  * check_num- function to help check if the integer input is a prime number
  *
  * @o: integer input to check a prime number
- * @p: integer input for divisor 
+ * @p: integer input for divisor
  *
- * Return: 1 if the input is a prime number, otherwise 0 if not prime 
+ * Return: 1 if the input is a prime number, otherwise 0 if not prime
  */
 int is_prime_number(int n)
 {
-	if ( n <= 1)
+	if (n <= 1)
 	{
 		return (0);
 	}
-	else if (prime_checker(n, n / 2) > 0 )
+	else if (prime_checker(n, n / 2) > 0)
 	{
 		return (1);
 	}
@@ -29,11 +29,11 @@ int prime_checker(int n, int o)
 	}
 	if (n % o == 0)
 	{
-		return(0);
+		return (0);
 	}
 	else
 	{
-		return (prime_checker(n, o - 1 ));
+		return (prime_checker(n, o - 1));
 	}
 }
 
