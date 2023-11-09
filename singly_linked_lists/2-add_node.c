f2345a9e38a4c80bed0ba75a65516e27fd69f181
root <schulaluke@gmail.com>
2023-11-09 09:16:53 -0800

    set variables and intialized arguments for file 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * list_t *add_node - a function that adds a new node at the beginning of a list_t list.
+ *
+ * @str: new string to add to the node
+ * @head: double pointer to the list_t list
+ *
+ * Return: Address of the new element, or NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_no;
+	unsigned int len = 0;
+
+	while(str[len])
+	{
+		len++;
+	}
+
