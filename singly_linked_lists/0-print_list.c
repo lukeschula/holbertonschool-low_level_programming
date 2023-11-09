304e1d3657137c47225754c1a6bab47683276d77
root <schulaluke@gmail.com>
2023-11-09 08:50:50 -0800

    added 0-main.c and #1 attempt at solving problem 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * size_t print-list - Write a function that prints all the elements of a list_t list.
+ *
+ * @h: pointer to the head pointer
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+	{
+		return(0);
+	}
+	
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}	 
