d9b3a3cb027f86a1679e0850655e68e94c211a06
root <schulaluke@gmail.com>
2023-11-10 06:43:50 -0800

    started on solving problem 4 and skipped poblem, whoops again

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lsuts.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * void free_list - a function that frees a list_t list
+ *
+ * @head: pointer to the head of the list_t
+ *
+ * Return: 0
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+	{
+		return (-1)
+	}
+	free (head);
+}
