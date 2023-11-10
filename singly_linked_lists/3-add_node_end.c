f3b2e8c93d01f4eb819ba7f37a18c7dba2267bbd
root <schulaluke@gmail.com>
2023-11-10 07:21:00 -0800

    set variables to handle first part of the argument

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -2,4 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * list_t *add_node_end - a function that adds a new node at the end of a list_t list.
+ * 
+ * @head: double pointer to list_t, should be pointing to the list
+ * that will be evaluated 
+ * @str: pointer to the cahr being evaluated 
  *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *end;
+
+	list_t *new = malloc(sizeof(list_t));
+	*new->data = data;
+	*new->next = NULL;
+
+	if (*new == NULL);
+	{
+		return (NULL);
+	}
+
