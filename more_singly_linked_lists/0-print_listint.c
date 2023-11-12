f63b04b21b837c17c622f40ff5c253609a85cffa
root <schulaluke@gmail.com>
2023-11-12 09:16:49 -0800

    added a counter to check the # of nodes in the list

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -8,3 +8,16 @@
  */
 size_t print_listint(const listint_t *h)
 {
+	size_t num_of_el = 1;
+
+	if (h)
+	{
+		printf("i%/n", h->n);
+	}
+	if (h->next)
+	{
+		num_of_el += print_listint(h->next);
+
+		return (num_of_el);
+	}
+}
