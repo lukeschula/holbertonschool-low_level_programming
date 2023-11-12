d679133f939f2967710d98e057a395a15629da67
root <schulaluke@gmail.com>
2023-11-12 09:52:30 -0800

    declared variables and allocated space for new node

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * add_nodeint - a function that adds a new node at the beginning of a listint_t list.
+ *
+ * @h: double pointer to the listint_t struct
+ * @n: integer input 
+ *
+ * Return: a new node with the head pointer pointing to it
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
