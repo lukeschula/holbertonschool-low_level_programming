724cfef2a6e91c8fadb7efdbc3d5f49e996c9226
root <schulaluke@gmail.com>
2023-11-12 12:02:16 -0800

    moved script from file 4 to file 5, didn't realize I was doing the wrong problem, lol

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -6,3 +6,20 @@
  * /
  */
 void free_listint2(listint_t **head)
+{
+        listint_t *ptr;
+
+        if (head == NULL)
+        {
+                return;
+        }
+        while (*head)
+        {
+                ptr = (*head)->next;
+                free(*head);
+                *head = ptr;
+
+        }
+        *head = NULL;
+}
+
