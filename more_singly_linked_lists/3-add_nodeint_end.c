79e599e7bd9cb89e2731ce1e062f7328057b82f9
root <schulaluke@gmail.com>
2023-11-12 10:11:07 -0800

    declared variables, allocated memory and if statement if allocation fails

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - function that adds a new node at the end of a listint_t list.
+ *
+ * @h: double pointer to the listint_t
+ * @n: integer input
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *end = *head;
+
+	new = malloc (sizeof(listint_t));
+
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
