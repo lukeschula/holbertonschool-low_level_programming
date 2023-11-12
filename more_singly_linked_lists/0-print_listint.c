10122d9e8e878b7406eed7889b8a51644d0d71bf
root <schulaluke@gmail.com>
2023-11-12 09:07:06 -0800

    added descriptio and prototype to file 0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_listint - a function that prints all the elements of a listint_t list.
+ *
+ * @h: pointer to the head pointer
+ *
+ * Return: the # of elements in listint_t list
+ */
+size_t print_listint(const listint_t *h)
+{
