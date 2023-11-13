dcd8d5532904ecfdbf41a6e0d5efac80bd5584d9
root <schulaluke@gmail.com>
2023-11-13 10:22:27 -0800

    created another for loop, going through the string multiplying v by 2 and if v is 1 it adds 1, return v for value

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -18,3 +18,11 @@ unsigned int binary_to_uint(const char *b)
 		if (b[x] != 0 || b[x] != 1)
 			return (0);
 	}
+	for (x = 0; b[x] != '\0'; x++;)
+	{
+		v *= 2;
+		if (b[x] == '1')
+			v += 1;
+	}
+	return (v);
+}
