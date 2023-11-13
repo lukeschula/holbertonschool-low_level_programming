3a6fd5906c75055640ecdb7990a19df38aeb647a
root <schulaluke@gmail.com>
2023-11-13 15:55:57 -0800

    file 1 debugging pt 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -28,11 +28,11 @@ int create_file(const char *filename, char *text_content)
 		{
 			i++;
 
-		l = write(f, text_content, i):
+		l = write(f, text_content, i);
 		}
 
 	}
-	if (close(f) == -1 ||i != l)
+	if (close(f) == -1 || i != l)
 	{
 		return (-1);
 
