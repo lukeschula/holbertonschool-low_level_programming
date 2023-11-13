8322ca7d985435d09c6fe44802cada1498f28cfc
root <schulaluke@gmail.com>
2023-11-13 15:54:35 -0800

    finished rough script for file 1, needs debugging

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -5,3 +5,37 @@
  *
  */
 int create_file(const char *filename, char *text_content)
+{
+	char *str;
+	int f;
+	int l = 0;
+	int i = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (f == -1)
+	{
+		return (-1);
+
+	}
+	if (text_content != NULL)
+	{
+		for (i = 0, str = text_content:*str, str++;)
+		{
+			i++;
+
+		l = write(f, text_content, i):
+		}
+
+	}
+	if (close(f) == -1 ||i != l)
+	{
+		return (-1);
+
+	return (1);
+	}
+}
