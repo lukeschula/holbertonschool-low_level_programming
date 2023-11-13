fdff6d979a4e17c26dc0290a392c596deaf8e7b6
root <schulaluke@gmail.com>
2023-11-13 15:01:21 -0800

    bitwise or operation, store in tmp< refixed logic

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -10,4 +10,13 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int x = 0;
 	unsigned int tmp;
 
+	tmp = n ^ m;
 
+	while (tmp)
+	{
+		if ((tmp & 1) == 1)
+			x++;
+		tmp >>= 1:
+
+	}
+}
