0681122aaa1340b47cb18ca11f3cb64df2741fae
root <schulaluke@gmail.com>
2023-11-13 15:30:12 -0800

    finished rough script for file 0, needs debugging

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,4 +5,41 @@
  *
  */
 ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t x, y;
+	char *ptr;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	ptr = malloc(sizeof(char) * letters);
+
+	if (ptr== NULL)
+	{
+		return (0);
+	}
+	x = open(filename, O_RDONLY);
+
+	if (x == -1)
+	{
+		return (0);
+	}
+	y = read(x, ptr, letters);
+
+	if (y == -1)
+	{
+		return (0);
+	}
+	y = write(STDOUT_FILENO, ptr, y;
+	
+	if (y == -1)
+	
+		return (0);
+	
+
+	close(x);
+
+	return (y);
+}
 
