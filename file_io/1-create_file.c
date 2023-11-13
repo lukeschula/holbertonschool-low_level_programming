e33479ba089ceb88f4001da78490b05c7d947dbd
root <schulaluke@gmail.com>
2023-11-13 15:57:03 -0800

    file 1 debugging pt 2

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (i = 0, str = text_content:*str, str++;)
+		for (i = 0, str = text_content; *str, str++)
 		{
 			i++;
 
