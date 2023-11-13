e5be21809807487b24d2bc0225bb69336057b7f1
root <schulaluke@gmail.com>
2023-11-13 15:34:45 -0800

    added libraries to main.h

--- file_io/main.h
+++ file_io/main.h
@@ -3,7 +3,10 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <fcntl.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 
 
@@ -16,3 +19,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+#endif
