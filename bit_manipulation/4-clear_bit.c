cd8fa615e91c55ea7c9e192711404b04f2dfd653
root <schulaluke@gmail.com>
2023-11-14 07:38:43 -0800

    test if I fixed file 4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -6,7 +6,7 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 88888888))
+	if (index >= (sizeof(unsigned long int) * 8))
 	{
 		return (-1);
 	}
