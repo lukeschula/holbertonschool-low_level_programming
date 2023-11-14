93d72526214c6c334fa0ec2a8d22b1f8aa25ecaa
root <schulaluke@gmail.com>
2023-11-13 17:51:14 -0800

    syntax fix

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,8 +9,14 @@ void print_binary(unsigned long int n)
 	if (n > 1)
 	{
 		print_binary(n >> 1);
-	
 
-	_putchar((n & 1) + '0');
 	}
+	if ((n & 1) == 1)
+	
+		_putchar(49);
+	
+	else
+	
+		_putchar(48);
+	
 }
