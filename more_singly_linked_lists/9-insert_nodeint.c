f624271309a93ee379b9412e1c1afb6108c8232c
root <schulaluke@gmail.com>
2023-11-13 18:39:54 -0800

    finally debugged file 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -6,9 +6,33 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int n;
-	listint_t *now;
-       	listint_t *str;
+	unsigned int ode;
+	listint_t *first, *second = *head;
 
-	*str = *head;	
+
+	first = malloc(sizeof(listint_t));
 	
+	if (first == NULL)
+	{
+		return (NULL);
+	}
+	first->n = n;
+	if (idx== 0)
+	{
+		first->next = second;
+		*head = first;
+		return (first);
+	}
+	for (ode = 0; ode < (idx - 1); ode++)
+	{
+		if (second == NULL || second->next == NULL)
+		{
+			return (NULL);
+		}
+		second = second->next;
+
+	}
+	first->next = second->next;
+	second->next = first;
+	return (first);
+}	
