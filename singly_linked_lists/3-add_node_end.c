f9cdaad70cb913ead0c88b1b2657f68573f1a8db
root <schulaluke@gmail.com>
2023-11-13 16:36:57 -0800

    betty style fix

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * list_t *add_node_end - a function that adds a new node at the end of a list_t list.
- * 
+ * list_t *add_node_end - a function that
+ * adds a new node at the end of a list_t list.
+ *
  * @head: double pointer to list_t, should be pointing to the list
- * that will be evaluated 
- * @str: pointer to the cahr being evaluated 
+ * that will be evaluated
+ * @str: pointer to the cahr being evaluated
  *
  * Return: the address of the new element, or NULL if it failed
  */
@@ -15,15 +16,15 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new;
 	list_t *end;
 	char *s;
-	int l; 
+	int l;
 
 	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
-	
+
 	s = strdup(str);
 
 	for (l = 0; s[l];)
@@ -40,7 +41,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new;
 	}
 	else
-	{	
+	{
 		end = *head;
 		while (end->next != NULL)
 		{
@@ -48,6 +49,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		}
 		end->next = new;
 	}
+	free(new);
 	return (*head);
 }
 
