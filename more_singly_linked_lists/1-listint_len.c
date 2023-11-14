652766726ce1293e5214f48defe12bcdebda3080
root <schulaluke@gmail.com>
2023-11-13 16:53:22 -0800

    kernel style fix

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * listint_len - function that returns 
+ * listint_len - function that returns
  * the number of elements in a linked listint_t list.
  *
  * @h: pointer to the head pointer
