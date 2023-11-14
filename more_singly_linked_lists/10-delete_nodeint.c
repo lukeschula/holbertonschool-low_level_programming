4286afe7e37c20a1a57a2adac19fe40612730a95
root <schulaluke@gmail.com>
2023-11-13 18:55:42 -0800

    almost done debugging file 10

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -7,4 +7,30 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int a = 0;
-	listin_t *ptr *phd
+	listin_t *ptr *phd = *head;
+
+	if (head == NULL)
+	{
+		return (-1)
+	}
+	if (index == 0)
+	{
+		*head = phd->next;
+		free(phd);
+		return (1);
+	}
+
+	while (phd != NULL && a < (index - 1))
+	{
+		phd = phd->next;
+		a++;
+	}
+	if (phd == NULL || phd->next == NULL)
+	
+		return (-1);
+	
+		ptr = phd->next;
+		phd->next = ptr->next;
+
+		return (1);
+}
