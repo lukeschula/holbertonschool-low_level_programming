1b0ad84230fbbd76c60735f835c6fa1329c2691d
root <schulaluke@gmail.com>
2023-11-13 17:44:38 -0800

    syntax fix

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	}
 	for (x = 0; b[x] != '\0'; x++)
 	{
-		if (b[x] != 0 && b[x] != 1)
+		if (b[x] != '0' && b[x] != '1')
 			return (0);
 	}
 	for (x = 0; b[x] != '\0'; x++)
