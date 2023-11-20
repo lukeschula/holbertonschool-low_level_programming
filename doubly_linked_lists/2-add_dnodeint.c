bb82d562d7615051ae20e4871b6e3f982cdb7022
root <schulaluke@gmail.com>
2023-11-20 13:49:27 -0800

    bug fix

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,18 +11,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	ptr = malloc(sizeof(dlistint_t));
 
 	if (ptr == NULL)
-	{
+	
 		return (NULL);
-	}
+	
 
 	ptr->n = n;
 	ptr->prev = NULL;
 	ptr->next = *head;
 
 	if (*head == NULL)
-	{
+	
 		(*head)->prev = ptr;
-	}
+	
 	*head = ptr;
 
 	return (ptr);
