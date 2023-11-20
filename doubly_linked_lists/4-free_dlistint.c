a46ced0e69e90cb11141d66c51c36a97072f4e03
root <schulaluke@gmail.com>
2023-11-20 12:08:06 -0800

    created rough script

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -5,3 +5,18 @@
  *
  */
 void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *freed = head;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	while (freed != NULL)
+	{
+		freed = head->next;
+		free(head)
+		ead = freead;
+	}
+}
+
