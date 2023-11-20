30e3d7517bf1c424bd506167a42b1a647e73be20
root <schulaluke@gmail.com>
2023-11-20 10:01:04 -0800

    added variables and allocated memory for the future pointer

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *present = NULL;
+	dlistint_t *future = NULL;
+
+	future = malloc(sizeof(dlistint_t));
+
+
+	
