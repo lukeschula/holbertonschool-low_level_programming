7eec2c67b73251a42ff9ca75cfe0d6e031148016
root <schulaluke@gmail.com>
2023-11-20 13:33:04 -0800

    completed scipt for file 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -6,4 +6,14 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	for 
+	for (; index != 0; index--)
+	{
+		if (head == NULL)
+		{
+			return (NULL);
+		}
+		head = head->next;
+	}
+
+	return (head);
+}
