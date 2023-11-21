55e863875fc7d150126acc5ef451508e1e2691e3
root <schulaluke@gmail.com>
2023-11-20 18:28:33 -0800

    debugged file 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -5,7 +5,7 @@
  *
  *
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {	
 	unsigned int locate_index;
 
