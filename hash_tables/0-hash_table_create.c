f31ee44f113933ea68d44dfde37d1960d282f7ca
root <schulaluke@gmail.com>
2023-11-20 17:52:09 -0800

    debugged file 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,4 +12,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
+	t = malloc(sizeof(hash_table_t));
 
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+	t->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (t->array == NULL)
+	{
+		free(t);
+		return (NULL);
+	}
+
+	t->size = size;
+
+	return (t);
