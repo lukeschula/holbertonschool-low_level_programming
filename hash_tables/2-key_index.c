435b3c45942670f420495ee72ac3e7201eb39cfa
root <schulaluke@gmail.com>
2023-11-20 18:29:23 -0800

    debugged file 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -9,7 +9,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {	
 	unsigned int locate_index;
 
-	locate_index = hash_d2b2(key);
+	locate_index = hash_djb2(key);
        	return (locate_index % size);
 }
 
