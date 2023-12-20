0cdb617569449f1211821e945c6af69c516b837d
root <schulaluke@gmail.com>
2023-12-20 10:33:47 -0800

    betty fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,8 +1,12 @@
 #include "search_algos.h"
 /**
+ * linear_search - function that searches for a value in an array of integers
+ * 
+ * @array: array of integers
+ * @size: size of the array
+ * @value: value to look for index
  *
- *
- *
+ * Return: index of value on success, -1 on failure
  */
 int linear_search(int *array, size_t size, int value)
 {
