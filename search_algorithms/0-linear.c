5671f9fa28a4bb8f424c515252c2d51ea5e7f18e
root <schulaluke@gmail.com>
2023-12-20 09:54:12 -0800

    i didn't recompile, im so dumb sometimes

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,18 +6,18 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t x = 0;
+	unsigned int y;
 
 	if (!array)
 		return (-1);
-	for (; x < size; x++)
+
+	for (y = 0; y < size; y++)
 	{
-		printf("Value hecked array[%ld] = [%d]\n",  x,  array[x]);
-		if (array[x] == value)
-			break;
+		printf("Value checked array[%u] = [%i]\n", y, array[y]);
+		if (array[y] == value)
+		{
+			return (y);
+		}
 	}
-	if (x == size)
-		return(-1);
-	
-	return (x);
+	return (-1);
 }
