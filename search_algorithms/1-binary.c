d0750f127ed1bfb1ed1b3180e445d73697af7d1e
root <schulaluke@gmail.com>
2023-12-20 10:15:43 -0800

    added rough script

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -16,3 +16,30 @@ void print_array(int *array, size_t l, size_t r)
 	}
 	printf("\n");
 }
+/**
+ *
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t l2, r2, mid;
+
+	if (array == NULL)
+		return (-1);
+
+	l2 = 0;
+	r2 = size - 1;
+	mid = 0;
+
+	while (l2 <= r2)
+	{
+		print_array(array, l2, r2);
+		mid = (l2 + r2) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			l2 = mid + 1;
+		else
+			r2 = mid - 1;
+	}
+	return (-1);
+}
