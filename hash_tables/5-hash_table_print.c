fc3d2e5f3704b8f594f005251b28f76b3116e02f
root <schulaluke@gmail.com>
2023-12-21 18:59:21 -0800

    betty fix

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,6 +1,8 @@
 #include "hash_tables.h"
 /**
- *
+ * hash_table_print - Prints a hash table
+ * @ht: A pointer to the hash table to print
+ * Return: void
  *
  */
 void hash_table_print(const hash_table_t *ht)
