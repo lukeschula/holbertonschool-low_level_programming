baf6c555c50a3637f008b61a5515f5f69585e6f5
root <schulaluke@gmail.com>
2023-12-21 19:24:06 -0800

    betty fix

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,8 +1,9 @@
 #include "hash_tables.h"
 /**
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
  *
- *
- *
+ * Return: NULL
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
