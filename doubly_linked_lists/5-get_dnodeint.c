41f56766063f7c38b79566a53c216eefa8ee255c
root <schulaluke@gmail.com>
2023-12-21 19:47:03 -0800

    betty fix

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 /**
- *
- *
- *
+ * get_dnodeint_at_index - funtion
+ * @head: pointer
+ * @index: index node to return
+ * Return: the nth node or if it does not exist, NULL
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
